a43c6497f2b4d626651a0c69f221a9f6e4834ae7
Simonbasco <simonbascobert@gmail.com>
2021-12-30 14:33:08 +0100

    Ajout methode question/reponse

--- ReserveTaPlaceConsole/ReserveTaPlaceConsole/Program.cs
+++ ReserveTaPlaceConsole/ReserveTaPlaceConsole/Program.cs
@@ -1,8 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleManage.Manager;
+using ConsoleManager.Data.Models;
 using ReserveTaPlace.Models;
 
 Console.WriteLine("Hello, World!");
+var manager = new Manager();
+var question1 = new Question("Quel film voulez vous ajouter ?", 0, null, null, QuestionType.ReponseLibre, null );
 
+manager.WriteQuestion(question1);
+var reponse = manager.ReadUserEntry(question1);
+Console.WriteLine(reponse.Text);
+Console.ReadLine();
 
-var movieProvider = new MovieProvider();
-await movieProvider.GetMovie("Dune");
+//var movieProvider = new MovieProvider();
+//await movieProvider.GetMovie("Dune");
--- ReserveTaPlaceConsole/ReserveTaPlaceConsole/Reader.cs
+++ ReserveTaPlaceConsole/ReserveTaPlaceConsole/Reader.cs
@@ -12,14 +12,14 @@ namespace ReserveTaPlaceConsole
     {
         public IWriter Writer { get; set; }
 
-        public Movie ReadMovie()
+        public Movie SearchMovie()
         {
             Writer.Display("Indiquer le film à ajouter :");
             var movieName = Console.ReadLine().Trim();
             if (String.IsNullOrEmpty(movieName))
             {
                 Writer.Display($"Le nom de film : {movieName} indiqué est incorrect");
-                return ReadMovie();
+                return SearchMovie();
             }
             var movie = new Movie("Alien", DateTime.Now);
             return movie ;
