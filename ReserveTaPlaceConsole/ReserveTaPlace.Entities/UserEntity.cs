cc32bddcb1088fae7c87758c4f7bde290035badf
julienGith <53513531+julienGith@users.noreply.github.com>
2022-01-12 14:53:58 +0100

    userEntity

---
+++ ReserveTaPlaceConsole/ReserveTaPlace.Entities/UserEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReserveTaPlace.Entities
+{
+    [Table("User")]
+    internal class UserEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Column("FirstName")]
+        public string FirstName { get; set; }
+        [Column("LastName")]
+        public string LastName { get; set; }
+        [Column("Email")]
+        public string Email { get; set; }
+        [Column("Password")]
+        public string Password { get; set; }
+        [Column("UserRoles")]
+        public UserRolesEntity UserRoles { get; set; }
+
+    }
+}
---
+++ ReserveTaPlaceConsole/ReserveTaPlace.Entities/UserRolesEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReserveTaPlace.Entities
+{
+    internal class UserRolesEntity
+    {
+        public enum UserRoles
+        {
+            Client,
+            AdminLocal,
+            AdminGroupe,
+            AdminGlobal
+        }
+    }
+}
