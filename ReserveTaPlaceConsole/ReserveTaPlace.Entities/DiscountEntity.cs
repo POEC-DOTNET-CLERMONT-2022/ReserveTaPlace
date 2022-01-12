a2fe9775b94991068f1af5e68c6774f7787d8309
Simonbasco <simonbascobert@gmail.com>
2022-01-12 18:43:40 +0100

    add DiscountEntity and DiscountTypeEntity

--- ReserveTaPlaceConsole/ReserveTaPlace.Entities/DiscountEntity.cs
+++ ReserveTaPlaceConsole/ReserveTaPlace.Entities/DiscountEntity.cs
@@ -1,14 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ReserveTaPlace.Entities
 {
+    [Table("Discount")]
     public class DiscountEntity
     {
+        [Key]
+        public int Id { get; set; }
         public int UserId { get; set; }
+        public int DiscountTypeId { get; set; }
+        public string Number { get; set; }
+        public DateOnly validityDate { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("Discounts")]
+        public virtual UserEntity User { get; set; }
+
+        [ForeignKey(nameof(DiscountTypeId))]
+        [InverseProperty("Discounts")]
+        public virtual DiscountTypeEntity DiscountType { get; set; }
+
 
     }
 }
--- ReserveTaPlaceConsole/ReserveTaPlace.Entities/DiscountTypeEntity.cs
+++ ReserveTaPlaceConsole/ReserveTaPlace.Entities/DiscountTypeEntity.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ReserveTaPlace.Entities
 {
-    internal class DiscountTypeEntity
+    [Table("DiscountType")]
+    public class DiscountTypeEntity
     {
+        public DiscountTypeEntity()
+        {
+            Discounts = new HashSet<DiscountEntity>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public short Rate { get; set; }
+        public float Amount { get; set; }
+
+        [InverseProperty(nameof(DiscountEntity.DiscountTypeId))]
+        public virtual ICollection<DiscountEntity> Discounts { get; set; }
     }
 }
