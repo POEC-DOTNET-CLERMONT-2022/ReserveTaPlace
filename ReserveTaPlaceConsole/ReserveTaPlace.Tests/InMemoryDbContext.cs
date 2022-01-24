820c21693d79adee96bdf2f014b1e29aacc4ece9
julienGith <53513531+julienGith@users.noreply.github.com>
2022-01-24 12:21:01 +0100

    Tests

---
+++ ReserveTaPlaceConsole/ReserveTaPlace.Tests/InMemoryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReserveTaPlace.Data.ApplicationContext;
+using ReserveTaPlace.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReserveTaPlace.API.Tests
+{
+    internal class InMemoryDbContext : ReserveTaPlaceContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+
+        public InMemoryDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- ReserveTaPlaceConsole/ReserveTaPlace.Tests/UserControllerTest.cs
+++ ReserveTaPlaceConsole/ReserveTaPlace.Tests/UserControllerTest.cs
@@ -1,10 +1,14 @@
+using AutoFixture;
 using AutoMapper;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using ReserveTaPlace.API.Controllers;
+using ReserveTaPlace.Data.Functions;
 using ReserveTaPlace.Data.Interfaces;
 using ReserveTaPlace.DTOS;
 using ReserveTaPlace.Entities;
@@ -17,18 +21,34 @@ namespace ReserveTaPlace.API.Tests
     [TestClass]
     public class UserControllerTest
     {
+        private Mock<IGenericRepo<UserEntity>> _mockGenericRepo;
         private UserController _userController;
         private IGenericRepo<UserEntity> _userGenericRepo;
+        public IEnumerable<UserEntity> CleanUsersDb;
         private ILogger<UserController> _logger { get; set; } = new NullLogger<UserController>();
         public IMapper Mapper;
+        public Fixture Fixture { get; set; }
         MapperConfiguration config;
-
-
+        InMemoryDbContext dbContext;
+        public UserControllerTest()
+        {
+            _mockGenericRepo = new Mock<IGenericRepo<UserEntity>>();
+            config = new MapperConfiguration(cfg => cfg.AddMaps(typeof(UserController)));
+            Mapper = new Mapper(config);
+            Fixture = new Fixture();
+        }
         [ClassInitialize]
         public void Init()
         {
-            config = new MapperConfiguration(cfg => cfg.AddMaps(typeof(UserController)));
-            Mapper = new Mapper(config);
+            var options = new DbContextOptionsBuilder().UseSqlite("Datasource=:InMemory").Options;
+            dbContext = new InMemoryDbContext(options);
+            dbContext.Database.Migrate();
+            CleanUsersDb = Fixture.CreateMany<UserEntity>(50);
+            dbContext.AddRange(CleanUsersDb);
+            dbContext.SaveChanges();
+            _userGenericRepo = new GenericFunctions<UserEntity>(dbContext);
+            _userController = new UserController(Mapper, _userGenericRepo, _logger);
+
         }
 
         //Test de Composant
@@ -38,8 +58,7 @@ namespace ReserveTaPlace.API.Tests
         {
             //Arrange
             #region Arrange
-            _userController = new UserController(Mapper, _userGenericRepo,_logger);
-            var result = await _userGenericRepo.GetAll();
+            var result = await _userController.GetAll();
 
 
             #endregion
@@ -53,7 +72,7 @@ namespace ReserveTaPlace.API.Tests
             //Assert
             #region Assert
             entities.Should().NotBeNull();
-            entities.Count().Should().Be(0);
+            entities.Count().Should().Be(50);
             #endregion
 
         }
