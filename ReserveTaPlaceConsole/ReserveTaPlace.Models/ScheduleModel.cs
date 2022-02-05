5accdfd4e8fd378fdde568bf368d88be448115ee
julienGith <53513531+julienGith@users.noreply.github.com>
2022-02-05 16:56:47 +0100

    update Session models

---
+++ ReserveTaPlaceConsole/ReserveTaPlace.Models/ScheduleModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReserveTaPlace.Models
+{
+    public class ScheduleModel
+    {
+        private Guid _id;
+        private DateTime _hourStart;
+        private DateTime _hourEnd;
+        public ScheduleModel(DateTime hourStart, DateTime hourEnd )
+        {
+            _id = Guid.NewGuid();
+            _hourStart = hourStart;
+            _hourEnd = hourEnd;
+        }
+        public Guid Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public DateTime HourStart
+        {
+            get { return _hourStart; }
+            set { _hourStart = value; }
+        }
+        public DateTime HourEnd
+        {
+            get { return _hourEnd; }
+            set { _hourEnd = value; }
+        }
+    }
+}
