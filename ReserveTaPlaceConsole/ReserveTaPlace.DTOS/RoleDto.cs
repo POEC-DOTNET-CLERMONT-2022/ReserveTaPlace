e13a36fa264d5ee57196545517b84154aee4172a
julienGith <53513531+julienGith@users.noreply.github.com>
2022-02-07 19:24:31 +0100

    updateTheater (#34)

--- ReserveTaPlaceConsole/ReserveTaPlace.DTOS/RoleDto.cs
+++ ReserveTaPlaceConsole/ReserveTaPlace.DTOS/RoleDto.cs
@@ -6,7 +6,7 @@ namespace ReserveTaPlace.DTOS
 {
     public class RoleDto
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
-
     }
 }
---
+++ ReserveTaPlaceConsole/ReserveTaPlace.DTOS/UserRoleDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReserveTaPlace.DTOS
+{
+    public class UserRoleDto
+    {
+        public Guid RoleId { get; set; }
+        public Guid UserId { get; set; }
+        public RoleDto Role { get; set; }
+    }
+}
